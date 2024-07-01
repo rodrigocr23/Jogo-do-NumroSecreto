76303595c1bcc98561983bf5f6058398e64a0884
cruvrodrigo <143622758+cruvrodrigo@users.noreply.github.com>
2024-07-01 14:21:09 -0300

    Add files via upload

---
+++ Desafios.js
@@ -0,0 +1,57 @@
+/*console.log('Olá Mundo!')
+
+let nome = prompt('Informe seu nome')
+
+function exibirNome() {
+    console.log(`Olá ${nome}!`);
+}
+
+exibirNome();
+
+// Doblo dos numeros
+let numeroDobro = parseFloat(prompt('Informe um Número'))
+function double() {
+    console.log(numeroDobro * 2)
+}
+
+double()
+
+// media entre os numeros
+let numeroDobro1 = parseFloat(prompt('Informe o primeiro Número'))
+let numeroDobro2 = parseFloat(prompt('Informe o Segundo Número'))
+let numeroDobro3 = parseFloat(prompt('Informe o Terceiro Número'))
+
+let media = (numeroDobro1 + numeroDobro2 + numeroDobro3)/3
+function mediaNumeros() {
+    console.log(media)
+}
+
+mediaNumeros()*/
+
+// Função para capturar um número válido do usuário
+/*
+function encontrarMaior(a, b) {
+    return a > b ? a : b;
+  }
+  
+  let maiorNumero = encontrarMaior(15, 9);
+  console.log(maiorNumero);*/
+
+// 2 maneiro
+/*
+if (numeroDobro1 >numeroDobro2) {
+    console.log(`O numero ${numeroDobro1} é maior que o ${numeroDobro2}`)
+}else{
+    console.log(`O numero ${numeroDobro2} é maior que o ${numeroDobro1}`)
+}
+*/
+/*
+// Multiplicação por ele mesmo
+let numeroM = parseFloat(prompt('Informe o primeiro Número'));
+let resultado = numeroM * numeroM;
+
+console.log(resultado)
+
+let listaGenerica = [];
+let liguagensDeProgramacao = ['javaScript','c','c++','Kotlin','Python'];
+liguagensDeProgramacao.push("Java","Ruby","GoLang");*/
